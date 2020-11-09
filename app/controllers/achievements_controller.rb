9f7f8c8ca45b682ff680489ad7139a4750c5c776
Asuka217 <asuka375@gmail.com>
2020-11-09 13:03:03 +0900

    コントローラーの修正・追記
    itemsとachievementsで、他ユーザーが自分の投稿の編集/達成報告ができないように追記と修正を行う。

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,5 +1,6 @@
 class AchievementsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :move_to_index, only: [:new, :create]
 
   def new
     @training = Training.find(params[:training_id])
@@ -20,5 +21,12 @@ class AchievementsController < ApplicationController
 
   def achievement_params
     params.require(:achievement).permit(:date_end, :report, :image).merge(user_id: current_user.id, training_id: (params[:training_id]))
+  end
+
+  def move_to_index
+    @training = Training.find(params[:training_id])
+    unless current_user.id == @training.user_id
+      redirect_to root_path
+    end
   end
 end
