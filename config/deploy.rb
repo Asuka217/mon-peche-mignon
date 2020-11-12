a6202b7c2f4b22e41a559ce7a65fb1971cbeaf53
Asuka217 <asuka375@gmail.com>
2020-11-12 14:57:31 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 lock "3.14.1"
 
 # Capistranoのログの表示に利用する
-set :applocation, 'mon-peche-mignon'
+set :application, 'mon-peche-mignon'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:Asuka217/mon-peche-mignon.git'
